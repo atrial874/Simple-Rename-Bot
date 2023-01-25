0ae053f8e7cfacfae1ee38da7d2f9c6e2959f399
atrial874 <95119887+atrial874@users.noreply.github.com>
2023-01-25 12:52:55 +0530

    Update utils.py

--- main/utils.py
+++ main/utils.py
@@ -12,19 +12,12 @@ async def progress_message(current, total, ud_type, message, start):
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)                                    
-        progress = "\n{0}{1}".format(
-            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
-            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))]))                                  
+        time_to_completion = round((total - current) / speed) * 1000                                  
         tmp = progress + PROGRESS_BAR.format(
             a=round(percentage, 2),
             b=humanbytes(current),
             c=humanbytes(total),
-            d=humanbytes(speed),
-            f=estimated_total_time if estimated_total_time != '' else "0 s")                               
+            d=humanbytes(speed),                               
         try:
             chance = [[InlineKeyboardButton("🚫 Cancel", callback_data="del")]]
             await message.edit(text="{}\n{}".format(ud_type, tmp), reply_markup=InlineKeyboardMarkup(chance))         
@@ -35,10 +28,6 @@ async def progress_message(current, total, ud_type, message, start):
 def humanbytes(size):
     units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
     size = float(size)
-    i = 0
-    while size >= 1024.0 and i < len(units):
-        i += 1
-        size /= 1024.0
     return "%.2f %s" % (size, units[i])
 
 
