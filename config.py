91c49c77039b0413674fb2f09596daa54e1be3d1
atrial874 <95119887+atrial874@users.noreply.github.com>
2023-01-25 12:46:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
 
 class temp(object):
     THUMBNAIL = environ.get("THUMBNAIL", "")
