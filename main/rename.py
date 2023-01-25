02bc5d57d1957a28d84e5eebcc164364ae335125
atrial874 <95119887+atrial874@users.noreply.github.com>
2023-01-25 13:28:38 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -7,25 +7,13 @@ from main.utils import progress_message, humanbytes
 @Client.on_message(filters.private & filters.command("rename") & filters.user(ADMIN))             
 async def rename_file(bot, msg):
     reply = msg.reply_to_message
-    if len(msg.command) < 2 or not reply:
-       return await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
     media = reply.document or reply.audio or reply.video
-    if not media:
-       await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
     og_media = getattr(reply, reply.media.value)
     new_name = msg.text.split(" ", 1)[1]
     sts = await msg.reply_text("Trying to Downloading.....")
     c_time = time.time()
     downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
     filesize = humanbytes(og_media.file_size)                
-    if CAPTION:
-        try:
-            cap = CAPTION.format(file_name=new_name, file_size=filesize)
-        except Exception as e:            
-            await sts.edit(text=f"Your caption Error unexpected keyword ●> ({e})")
-            return
-    else:
-        cap = f"{new_name}\n\n💽 size : {filesize}"
     raw_thumbnail = temp.THUMBNAIL 
     if raw_thumbnail:
         og_thumbnail = await bot.download_media(raw_thumbnail)
